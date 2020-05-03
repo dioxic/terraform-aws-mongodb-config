ad89cd6b2f876d787fe0038a4fe57224033ac89d
Mark Baker-Munton <dioxic@gmail.com>
2020-05-03 16:50:06 +0100

    examples added

---
+++ examples/replicaset/main.tf
@@ -0,0 +1,21 @@
+module "config" {
+  source = "../.."
+
+  member_count = 3
+  name = "markbm"
+  image_id = "ami-06ce3edf0cff21f07"
+  instance_type = "t2.micro"
+  domain_name = "example.com"
+}
+
+output "data_replica_sets" {
+  value = module.config.data_replica_sets
+}
+
+output "config_replica_set" {
+  value = module.config.config_replica_set
+}
+
+output "router_nodes" {
+  value = module.config.router_nodes
+}
